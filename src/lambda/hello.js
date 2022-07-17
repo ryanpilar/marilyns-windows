7b036e0d9c51f9172a879aa5229dc15725dd15dc
ryanpilar <ryan_pilar@outlook.com>
2022-07-17 13:25:03 -0600

    updated lambda hello

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,5 +1,6 @@
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 export function handler(event, context, callback) {
+    console.log("hello world");
     callback(null, {
         statusCode: 200,
         // That's the status code which will be returned when the function is executed to indicates that everything is OK
