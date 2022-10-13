c5905d7edf04ad68430a95ad5d1a8bff1c456706
ryanpilar <ryan_pilar@outlook.com>
2022-10-13 01:29:12 -0600

    testamonial troubleshooting test

--- src/containers/testimonial/layout-01/index.jsx
+++ src/containers/testimonial/layout-01/index.jsx
@@ -44,6 +44,7 @@ const TestimonialArea = ({ data }) => {
                 >
                     {data.items?.map((testimonial) => (
                         <SwiperSlide className="item" key={testimonial.id}>
+                            <h3>{testimonial.name}</h3>
                             <Testimonial
                                 authorName={testimonial.name}
                                 authroRole={testimonial.designation}
